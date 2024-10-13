b3526c0366fb4dcec2fde01546ce7ce4e246d449
Hass Khalid <hass.khalid@agilebits.com>
2024-10-13 08:25:29 +0100

    enable sudo with touch id

--- hosts/common/core/general.nix
+++ hosts/common/core/general.nix
@@ -4,4 +4,5 @@
   system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
   system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = 3.0;
   system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = 3.0;
+  security.pam.enableSudoTouchIdAuth = true;
 }
