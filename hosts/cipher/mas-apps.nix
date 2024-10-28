1bd1462cb41425773bac6a2987ab821b411190ce
Hass Khalid <me@razgriz.io>
2024-10-28 15:03:43 +0000

    enabled mas apps on cipher

--- hosts/cipher/mas-apps.nix
+++ hosts/cipher/mas-apps.nix
@@ -1,9 +1,9 @@
 { ... }:
 {
-  # homebrew = {
-  #   masApps = {
-  #     Lungo = 1263070803;
-  #     "World Clock Pro — Convert Time" = 858446756;
-  #   };
-  # };
+  homebrew = {
+    masApps = {
+      Lungo = 1263070803;
+      "World Clock Pro — Convert Time" = 858446756;
+    };
+  };
 }
