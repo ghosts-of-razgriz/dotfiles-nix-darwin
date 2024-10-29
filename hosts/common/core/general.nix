604243f8cc76f63deaf48ce844e51769074b5205
Hass Khalid <me@razgriz.io>
2024-10-29 08:10:10 +0000

    system defaults for spoken content voice

--- hosts/common/core/general.nix
+++ hosts/common/core/general.nix
@@ -5,10 +5,33 @@
   system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = 3.0;
   system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = 3.0;
 
+  system.defaults.CustomUserPreferences = {
+    "com.apple.assistant.backedup" = {
+      "Output Voice" = {
+        Custom = 1;
+        Footprint = 2;
+        Gender = 2;
+        Language = "en-US";
+        Name = "nora";
+      };
+    };
+
+    "com.apple.Accessibility" = {
+      SpokenContentDefaultVoiceSelectionsByLanguage = [
+        "en"
+        {
+          "_type" = "Speech.VoiceSelection";
+          "_version" = 0;
+          voiceId = "com.apple.ttsbundle.gryphon-neural_Nora_en-US_premium";
+        }
+      ];
+    };
+  };
+
   security.pam.enableSudoTouchIdAuth = true;
   environment.etc."pam.d/sudo_local".text = ''
     # Managed by Nix Darwin
-	auth      optional      ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
-	auth      sufficient    pam_tid.so
+    auth      optional      ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+    auth      sufficient    pam_tid.so
   '';
 }
