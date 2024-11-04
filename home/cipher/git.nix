b5ed8a8dff629d1c66c6498a5a483169cfcc8ed1
Hass Khalid <me@razgriz.io>
2024-11-04 16:55:32 +0000

    git ignore devenv files on cipher

--- home/cipher/git.nix
+++ home/cipher/git.nix
@@ -1,6 +1,20 @@
 { ... }:
 {
   programs.git.includes = [
-    { contents.user.email = "hass.khalid@agilebits.com"; }
+    {
+      contents = {
+        user.email = "hass.khalid@agilebits.com";
+        ignores = [
+          ".envrc"
+          ".devenv*"
+          ".direnv"
+          ".pre-commit-config.yaml"
+          "devenv.nix"
+          "devenv.local.nix"
+          "devenv.lock"
+          "devenv.yaml"
+        ];
+      };
+    }
   ];
 }
