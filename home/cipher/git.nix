d74ea2cb51d128d66eda5bd28e687f434d627e55
Hass Khalid <hass.khalid@agilebits.com>
2024-11-05 15:46:27 +0000

    corrected the git config for cipher

--- home/cipher/git.nix
+++ home/cipher/git.nix
@@ -1,20 +1,22 @@
 { ... }:
 {
-  programs.git.includes = [
-    {
-      contents = {
-        user.email = "hass.khalid@agilebits.com";
-        ignores = [
-          ".envrc"
-          ".devenv*"
-          ".direnv"
-          ".pre-commit-config.yaml"
-          "devenv.nix"
-          "devenv.local.nix"
-          "devenv.lock"
-          "devenv.yaml"
-        ];
-      };
-    }
-  ];
+  programs.git = {
+    includes = [
+      {
+        contents = {
+          user.email = "hass.khalid@agilebits.com";
+        };
+      }
+    ];
+    ignores = [
+      ".envrc"
+      ".devenv*"
+      ".direnv"
+      ".pre-commit-config.yaml"
+      "devenv.nix"
+      "devenv.local.nix"
+      "devenv.lock"
+      "devenv.yaml"
+    ];
+  };
 }
