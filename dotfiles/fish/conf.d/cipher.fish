162973f14cdabc56f8457fadcf311c6529343fda
Hass Khalid <me@razgriz.io>
2025-01-07 12:37:43 +0000

    work env vars

---
+++ dotfiles/fish/conf.d/cipher.fish
@@ -0,0 +1,5 @@
+set --export GITLAB_TOKEN "op://Employee/Gitlab/PAT/b5-token"
+
+set --export GONOPROXY "gitlab.1password.io,go.1password.io"
+set --export GONOSUMDB "gitlab.1password.io,go.1password.io"
+set --export GOPRIVATE "gitlab.1password.io,go.1password.io"
