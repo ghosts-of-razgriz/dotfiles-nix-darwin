4fc63c89f72df90401fe0604930178e2be311d9a
Hass Khalid <me@razgriz.io>
2025-01-07 12:35:33 +0000

    work gitconfig update

--- home/cipher/git.nix
+++ home/cipher/git.nix
@@ -5,6 +5,9 @@
       {
         contents = {
           user.email = "hass.khalid@agilebits.com";
+          "url \"ssh://git@ssh.gitlab.1password.io:2227\"" = {
+            insteadOf = "https://gitlab.1password.io";
+          };
         };
       }
     ];
