dacc8d4afc395e12b79a417595537a538287c5b1
Hass Khalid <me@razgriz.io>
2025-03-02 19:52:20 +0000

    nix update

--- hosts/common/core/general.nix
+++ hosts/common/core/general.nix
@@ -41,7 +41,7 @@
     };
   };
 
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   environment.etc."pam.d/sudo_local".text = ''
     # Managed by Nix Darwin
     auth      optional      ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
