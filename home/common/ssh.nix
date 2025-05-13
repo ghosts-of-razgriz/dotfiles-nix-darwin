2b26e79a6aef78ba1cd684bce044cfae7e183625
Hass Khalid <me@razgriz.io>
2025-05-13 02:14:14 +0100

    ssh config

---
+++ home/common/ssh.nix
@@ -0,0 +1,20 @@
+{ config, configVars, ... }:
+{
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    '';
+    includes = [
+      "${configVars.hostname}.config"
+    ];
+  };
+
+  home.file.".ssh/${configVars.hostname}.config" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.dotfilesPath}/ssh/${configVars.hostname}.config";
+  };
+
+  home.file.".ssh/authorized_keys" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.dotfilesPath}/ssh/authorized_keys";
+  };
+}
