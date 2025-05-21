1a8c933b14fa194ba84698fc58f6a6fcf842b8c3
Hass Khalid <hass.khalid@agilebits.com>
2025-05-21 14:53:50 +0100

    git settings for cipher

--- home/cipher/git.nix
+++ home/cipher/git.nix
@@ -1,6 +1,15 @@
 { ... }:
 {
   programs.git.includes = [
-    { contents.user.email = "hass.khalid@agilebits.com"; }
+    {
+      contents.user.email = "hass.khalid@agilebits.com";
+      contents."url \"ssh://git@site1.gitlab.1password.io:2227\"" = {
+        insteadOf = [
+          "https://gitlab.1password.io"
+          "ssh://git@ssh.gitlab.1password.op:2227"
+          "ssh://git@gitlab.1passowrd.io:2227"
+        ];
+      };
+    }
   ];
 }
