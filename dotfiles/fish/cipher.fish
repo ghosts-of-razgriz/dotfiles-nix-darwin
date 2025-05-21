759c17baa68fa2c41b97b5409315b8e34a3dbf84
Hass Khalid <hass.khalid@agilebits.com>
2025-05-21 14:54:15 +0100

    envars for cipher

--- dotfiles/fish/cipher.fish
+++ dotfiles/fish/cipher.fish
@@ -1,1 +1,9 @@
 set --export GITLAB_TOKEN "op://Employee/Gitlab/PAT/b5"
+
+set --export GONOPROXY "gitlab.1password.io,go.1password.io"
+set --export GONOSUMDB "gitlab.1password.io,go.1password.io"
+set --export GOPRIVATE "gitlab.1password.io,go.1password.io"
+
+set --export GOPATH "$HOME/.config/go"
+
+fish_add_path $GOPATH/bin
