348cc47192bb7a01ef4f8cc6b332143a99c12785
Hass Khalid <me@razgriz.io>
2025-10-05 04:25:22 +0100

    general ssh keys config

--- home/common/default.nix
+++ home/common/default.nix
@@ -68,6 +68,11 @@
       source = config.lib.file.mkOutOfStoreSymlink "${config.dotfilesPath}/jujutsu/${configVars.hostname}.toml";
       force = true;
     };
+
+    xdg.configFile."1Password/ssh/agent.toml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.dotfilesPath}/1password/${configVars.hostname}.agent.toml";
+      force = true;
+    };
   };
 
   imports = [
--- home/yellow13/default.nix
+++ home/yellow13/default.nix
@@ -1,11 +1,6 @@
 { config, configVars, ... }:
 {
 
-  xdg.configFile."1Password/ssh/agent.toml" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.dotfilesPath}/1password/${configVars.hostname}.agent.toml";
-    force = true;
-  };
-
   imports = [
     ../common
     ./git.nix
