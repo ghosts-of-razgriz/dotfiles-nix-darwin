ad438554ad0efbdd3463dd8f6501c29af7dadac5
Hass Khalid <me@razgriz.io>
2025-10-05 04:09:48 +0100

    ssh config for yellow13

--- home/yellow13/default.nix
+++ home/yellow13/default.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ config, configVars, ... }:
 {
+
+  xdg.configFile."1Password/ssh/agent.toml" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.dotfilesPath}/1password/${configVars.hostname}.agent.toml";
+    force = true;
+  };
 
   imports = [
     ../common
